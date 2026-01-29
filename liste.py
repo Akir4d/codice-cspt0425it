f0ff09ba2f034f11beaa4fd4d03c1e5c6b9b174c
Paolo Rampino <you@example.com>
2026-01-29 14:44:42 -0500

    UP

--- liste.py
+++ liste.py
@@ -1,4 +1,13 @@
 lista = [12, 34, 54, "ciao", True, [1,2]]
 
-print(lista[0])
-print(lista[5])
+print(lista)
+lista[0] = "Questo era 0"
+print(lista)
+lista.append("Nuovo elemento")
+print(lista)
+lista.insert(1, "33")
+print(lista)
+del lista[0]
+print(lista)
+# print(lista[0])
+#for i in lista: print(i)
