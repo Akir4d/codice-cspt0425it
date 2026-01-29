de45071dcfd86865b48f9f92112002d74cbe88f1
Paolo Rampino <you@example.com>
2026-01-29 14:04:17 -0500

    UP

---
+++ ifelse.py
@@ -0,0 +1,10 @@
+x = int(input("Dammi un numero: "))
+
+if (x > 10):
+    print("Il numero e' maggiore di 10")
+elif (x == 10):
+    print("il numero e' uguale a 10")
+elif (x < 0):
+    print("il numero e' negativo")
+else:
+    print("Il numero non e' maggiore di 10")
