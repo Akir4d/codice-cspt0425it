a2b470b37b59798bfcafe16519292f171f1fcb1a
Paolo Rampino <liadserv@gmail.com>
2026-02-02 15:29:49 -0500

    up

---
+++ scansione.py
@@ -0,0 +1,25 @@
+import socket as so
+
+target = input("Inserisci un ip da scansire: ")
+portre = input("Da che porta a che porta scasire nel formato bassa-alta (es: 0-100)? ")
+
+# ricavo la porta bassa suddividento la stringa in un tupla di due elementi
+# per separe la stringa utilizzero' il carattere -
+
+lowport = int(portre.split('-')[0])
+# stessa cosa con la porta alta
+highpor = int(portre.split('-')[1])
+
+print(f"Scansisco {target} dalla porta {lowport} alla porta {highpor}")
+
+for port in range(lowport, highpor+1):
+    # connessione ipv4 tcp
+    s = so.socket(so.AF_INET, so.SOCK_STREAM)
+    # configuriamo una connessione semplice con ip della macchina target
+    # e porta che cambia sempre grazie al ciclo for e range
+    status = s.connect_ex((target, port))
+    if (status == 0):
+        print(f"*** Porta {port} aperta ***")
+    else:
+        print(f"Porta {port} chiusa")
+    s.close()
