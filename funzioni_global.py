eba45944efd83e6e453c12b44047c6b1f840f00c
Paolo Rampino <liadserv@gmail.com>
2026-02-02 13:36:53 -0500

    UP

--- funzioni_global.py
+++ funzioni_global.py
@@ -1,10 +1,16 @@
 x = 10
 
 def test_scope(z):
+    '''Ciao''' # vangono usati come commenti ma sono stringhe
+    """Come""" # Perche' possono andare a capo
     global x # questo da il permesso alla funzione di usare la variabile in lettuara/scrittura
     x = x + z
     return x
 
+"""
+Questo e' un modo
+per scrivere un commento
+"""
 
 print("La x prima della funzione e' ", x)
 print("Il valore di test_scope e' ", test_scope(2))
